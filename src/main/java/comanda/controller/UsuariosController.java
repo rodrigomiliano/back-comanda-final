86375d6c9d467e0b1da40e3b953eb185484bfec6
emveloso-src <emiliano.veloso@gmail.com>
2023-12-20 12:14:02 -0300

    update

--- src/main/java/comanda/controller/UsuariosController.java
+++ src/main/java/comanda/controller/UsuariosController.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -47,11 +46,11 @@ public class UsuariosController {
 	}
 
 	@PostMapping("/login")
-	public ResponseEntity<Usuario> guardar(@RequestBody FormLogin formLogin) throws RolNoEncontradoException {
+	public Usuario guardar(@RequestBody FormLogin formLogin) throws RolNoEncontradoException {
 		try {
-			return ResponseEntity.ok(serviceUsuarios.login(formLogin));
+			return serviceUsuarios.login(formLogin);
 		} catch (UsuarioNotFoundException | InvalidPasswordException e) {
-			return ResponseEntity.badRequest().body(null);
+			return null;
 		}
 	}
 
