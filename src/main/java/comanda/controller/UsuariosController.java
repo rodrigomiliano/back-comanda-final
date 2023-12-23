4291d27daf00785b8d737ba73cdcdcdb8e16dee1
emveloso-src <emiliano.veloso@gmail.com>
2023-12-23 08:27:21 -0300

    update

--- src/main/java/comanda/controller/UsuariosController.java
+++ src/main/java/comanda/controller/UsuariosController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -52,11 +51,6 @@ public class UsuariosController {
 		} catch (UsuarioNotFoundException | InvalidPasswordException e) {
 			return null;
 		}
-	}
-
-	@PutMapping("/usuario")
-	public Usuario modificar(@RequestBody FormUsuario formUsuario) throws RolNoEncontradoException {
-		return serviceUsuarios.guardar(formUsuario);
 	}
 
 	@DeleteMapping("/usuario/{id}")
